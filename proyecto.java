a0a621acd2cdbac19edd6785fdc102d439b139b7
Marina Alfaro <marinaalfaromartin11@gmail.com>
2020-11-17 12:21:23 +0100

    proyecto.java

---
+++ proyecto.java
@@ -0,0 +1,12 @@
+public class Hasta {
+	public static void main (String[] args) {
+		int suma=0;
+		int n=10:
+		int cont=0;
+		do {
+			suma+=cont;
+			cont++;
+		} while (cont<=n);
+		System.out.println("="+suma)
+	}
+}
